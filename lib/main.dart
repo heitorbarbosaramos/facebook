18c6f36ad050858b975f89c4449154725ea10006
heitor.ramos <heitorhfbr@gmail.com>
2022-01-17 10:46:13 -0300

    Projeto criado

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:interface_facebook/telas/Home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Facebook",
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
+}
