149ec8a13989771d911b6200061593c8f65b43ca
heitor.ramos <heitorhfbr@gmail.com>
2022-01-19 12:28:18 -0300

    Adcionando visualizacao com DevicePreview

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:interface_facebook/telas/Home.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "Facebook",
-    debugShowCheckedModeBanner: false,
-    home: Home(),
-  ));
+  runApp(
+      DevicePreview(
+          builder: (context) => MaterialApp(
+            title: "Facebook",
+            debugShowCheckedModeBanner: false,
+
+            builder: DevicePreview.appBuilder,
+            locale: DevicePreview.locale(context),
+
+            home: Home(),
+          )
+        ),
+      );
 }
